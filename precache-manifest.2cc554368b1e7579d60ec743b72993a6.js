375ae95f194d9bd7cc30863a1d5f4e18be15d55f
unknown <iaguiarg@hotmail.com>
2019-08-21 12:59:35 -0500

    Updates

---
+++ precache-manifest.2cc554368b1e7579d60ec743b72993a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15f19858108792a58aed856d9f13ef5",
+    "url": "/React-Redux/index.html"
+  },
+  {
+    "revision": "53a640152f0a10a70465",
+    "url": "/React-Redux/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "24b2fb2ea2a8d12088be",
+    "url": "/React-Redux/static/css/main.435b70e2.chunk.css"
+  },
+  {
+    "revision": "53a640152f0a10a70465",
+    "url": "/React-Redux/static/js/2.48814879.chunk.js"
+  },
+  {
+    "revision": "24b2fb2ea2a8d12088be",
+    "url": "/React-Redux/static/js/main.a6a1114f.chunk.js"
+  },
+  {
+    "revision": "5572492fd94cafb02f66",
+    "url": "/React-Redux/static/js/runtime~main.a7e0179d.js"
+  }
+]);
